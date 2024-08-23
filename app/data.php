741eda1a7978f8bb7aa3cdefb557cbc5dae391b8
Nurunnahar <nurunnahar9081@gmail.com>
2024-08-23 12:38:14 +0600

    about section dynamic

---
+++ app/data.php
@@ -0,0 +1,12 @@
+<?php
+
+$about = [
+    "title" => "About Us ",
+    "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo
+maxime voluptatem a itaque suscipit.",
+    "button" => [
+        "text" => "About Us",
+        "link" => "#",
+    ],
+    "photo" => "https://cdn.pixabay.com/photo/2016/12/13/22/25/bird-1905255_640.jpg",
+];
---
+++ autoload.php
@@ -0,0 +1,13 @@
+<?php
+// date_default_timezone_set("Asia/Dakha");
+
+if(file_exists(__DIR__.'/app/functions.php')){
+    require_once __DIR__.'/app/functions.php';
+}
+
+if(file_exists(__DIR__.'/app/data.php')){
+    require_once __DIR__.'/app/data.php';
+}
+
+
+ 
