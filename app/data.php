2c0d79a2c6d2cec025944d6537fa4522109c6228
Nurunnahar <nurunnahar9081@gmail.com>
2024-08-23 23:33:48 +0600

    slider section done

--- app/data.php
+++ app/data.php
@@ -10,3 +10,57 @@ maxime voluptatem a itaque suscipit.",
     ],
     "photo" => "https://cdn.pixabay.com/photo/2016/12/13/22/25/bird-1905255_640.jpg",
 ];
+$slider = [
+    [
+        "title" => "For All Your
+        Furniture Needs 1",
+        "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis,
+        illo maxime voluptatem a itaque suscipit. ",
+        "buttons" => [
+            "contact_button" =>[
+                "title"=>"Contact Us",
+                "link"=> "#"
+            ],
+            "about_button" =>[
+                "title"=>"About Us",
+                "link"=> "#"
+            ],
+        ],
+        "photo" => "https://t4.ftcdn.net/jpg/05/65/36/03/360_F_565360370_LrWWCTxczrmwqpsPYPljiFyE4gFqpecr.jpg"
+    ],
+    [
+        "title" => "For All Your
+        Furniture Needs 2",
+        "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis,
+        illo maxime voluptatem a itaque suscipit. ",
+        "buttons" => [
+            "contact_button" =>[
+                "title"=>"Contact Us",
+                "link"=> "#"
+            ],
+            "about_button" =>[
+                "title"=>"About Us",
+                "link"=> "#"
+            ],
+        ],
+        "photo" =>
+        "https://static.vecteezy.com/system/resources/thumbnails/023/122/855/small_2x/colorful-bird-on-a-background-of-nature-ai-generated-free-photo.jpg"
+    ],
+    [
+        "title" => "For All Your
+        Furniture Needs 3",
+        "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis,
+        illo maxime voluptatem a itaque suscipit. ",
+        "buttons" => [
+            "contact_button" =>[
+                "title"=>"Contact Us",
+                "link"=> "#"
+            ],
+            "about_button" =>[
+                "title"=>"About Us",
+                "link"=> "#"
+            ],
+        ],
+        "photo" => "https://img.freepik.com/free-photo/bird-with-blue-head-red-head-sits-pink-flower_1340-22892.jpg"
+    ],
+];
--- index.php
+++ index.php
@@ -94,96 +94,33 @@
     <section class="slider_section long_section">
       <div id="customCarousel" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="container ">
-              <div class="row">
-                <div class="col-md-5">
-                  <div class="detail-box">
-                    <h1>
-                      For All Your <br>
-                      Furniture Needs
-                    </h1>
-                    <p>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo maxime voluptatem a itaque suscipit.
-                    </p>
-                    <div class="btn-box">
-                      <a href="" class="btn1">
-                        Contact Us
-                      </a>
-                      <a href="" class="btn2">
-                        About Us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-7">
-                  <div class="img-box">
-                    <img src="images/slider-img.png" alt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="container ">
-              <div class="row">
-                <div class="col-md-5">
-                  <div class="detail-box">
-                    <h1>
-                      For All Your <br>
-                      Furniture Needs
-                    </h1>
-                    <p>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo maxime voluptatem a itaque suscipit.
-                    </p>
-                    <div class="btn-box">
-                      <a href="" class="btn1">
-                        Contact Us
-                      </a>
-                      <a href="" class="btn2">
-                        About Us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-7">
-                  <div class="img-box">
-                    <img src="images/slider-img.png" alt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="container ">
-              <div class="row">
-                <div class="col-md-5">
-                  <div class="detail-box">
-                    <h1>
-                      For All Your <br>
-                      Furniture Needs
-                    </h1>
-                    <p>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo maxime voluptatem a itaque suscipit.
-                    </p>
-                    <div class="btn-box">
-                      <a href="" class="btn1">
-                        Contact Us
-                      </a>
-                      <a href="" class="btn2">
-                        About Us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-7">
-                  <div class="img-box">
-                    <img src="images/slider-img.png" alt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <?php foreach($slider as $slide): ?>
+               <div class="carousel-item <?php echo $slider[0]['title'] == $slide['title'] ? 'active' : ''; ?> ">
+                 <div class="container ">
+                   <div class="row">
+                     <div class="col-md-5">
+                       <div class="detail-box">
+                         <h1><?php echo $slide['title']; ?></h1>
+                         <p><?php echo $slide['description']; ?></p>
+                         <div class="btn-box">
+                           <a href="<?php echo $slide['buttons']['contact_button']['link']; ?>"
+                             class="btn1"><?php echo $slide['buttons']['contact_button']['title']; ?></a>
+                           <a href="<?php echo $slide['buttons']['about_button']['link']; ?>"
+                             class="btn2"><?php echo $slide['buttons']['about_button']['title']; ?> </a>
+                         </div>
+                       </div>
+                     </div>
+                     <div class="col-md-7">
+                       <div class="img-box">
+                         <img src="<?php echo $slide['photo']; ?>" alt="">
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+          <?php endforeach ; ?>
+         
+
         </div>
         <ol class="carousel-indicators">
           <li data-target="#customCarousel" data-slide-to="0" class="active"></li>
