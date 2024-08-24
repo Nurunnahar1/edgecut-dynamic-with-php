fa9277c3b33b85296a1d42fb6b82bee253418abd
Nurunnahar <nurunnahar9081@gmail.com>
2024-08-24 11:11:23 +0600

    Birds  section done

--- app/data.php
+++ app/data.php
@@ -13,54 +13,121 @@ maxime voluptatem a itaque suscipit.",
 $slider = [
     [
         "title" => "For All Your
-        Furniture Needs 1",
+        Bird Needs 1",
         "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis,
         illo maxime voluptatem a itaque suscipit. ",
         "buttons" => [
-            "contact_button" =>[
-                "title"=>"Contact Us",
-                "link"=> "#"
+            "contact_button" => [
+                "title" => "Contact Us",
+                "link" => "#",
             ],
-            "about_button" =>[
-                "title"=>"About Us",
-                "link"=> "#"
+            "about_button" => [
+                "title" => "About Us",
+                "link" => "#",
             ],
         ],
-        "photo" => "https://t4.ftcdn.net/jpg/05/65/36/03/360_F_565360370_LrWWCTxczrmwqpsPYPljiFyE4gFqpecr.jpg"
+        "photo" => "https://t4.ftcdn.net/jpg/05/65/36/03/360_F_565360370_LrWWCTxczrmwqpsPYPljiFyE4gFqpecr.jpg",
     ],
     [
         "title" => "For All Your
-        Furniture Needs 2",
+            Bird Needs 2",
         "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis,
-        illo maxime voluptatem a itaque suscipit. ",
+            illo maxime voluptatem a itaque suscipit. ",
         "buttons" => [
-            "contact_button" =>[
-                "title"=>"Contact Us",
-                "link"=> "#"
+            "contact_button" => [
+                "title" => "Contact Us",
+                "link" => "#",
             ],
-            "about_button" =>[
-                "title"=>"About Us",
-                "link"=> "#"
+            "about_button" => [
+                "title" => "About Us",
+                "link" => "#",
             ],
         ],
         "photo" =>
-        "https://static.vecteezy.com/system/resources/thumbnails/023/122/855/small_2x/colorful-bird-on-a-background-of-nature-ai-generated-free-photo.jpg"
+        "https://static.vecteezy.com/system/resources/thumbnails/023/122/855/small_2x/colorful-bird-on-a-background-of-nature-ai-generated-free-photo.jpg",
     ],
     [
         "title" => "For All Your
-        Furniture Needs 3",
+            Bird Needs 3",
         "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis,
-        illo maxime voluptatem a itaque suscipit. ",
+            illo maxime voluptatem a itaque suscipit. ",
         "buttons" => [
-            "contact_button" =>[
-                "title"=>"Contact Us",
-                "link"=> "#"
+            "contact_button" => [
+                "title" => "Contact Us",
+                "link" => "#",
             ],
-            "about_button" =>[
-                "title"=>"About Us",
-                "link"=> "#"
+            "about_button" => [
+                "title" => "About Us",
+                "link" => "#",
             ],
         ],
-        "photo" => "https://img.freepik.com/free-photo/bird-with-blue-head-red-head-sits-pink-flower_1340-22892.jpg"
+        "photo" => "https://img.freepik.com/free-photo/bird-with-blue-head-red-head-sits-pink-flower_1340-22892.jpg",
+    ],
+];
+
+$birds = [
+    "title" => "OUR BIRDS",
+    "description" => "Birds name with pictures and details, definition and example",
+    "single_bird" => [
+        [
+            "photo" => "https://www.activewild.com/wp-content/uploads/2018/10/wandering-albatross.jpg",
+            "name" => "wandering albatross",
+            "price" => "100.00",
+            "button" => [
+                "title" => "BUY NOW",
+                "link" => "#",
+            ],
+        ],
+        [
+            "photo" => "https://i.dailymail.co.uk/i/pix/2015/06/03/22/0A5A3807000005DC-0-image-a-31_1433368719400.jpg",
+            "name" => "Kingfisher",
+            "price" => "150.00",
+            "button" => [
+                "title" => "BUY NOW",
+                "link" => "#",
+            ],
+        ],
+        [
+            "photo" =>
+            "https://cdn.firstcry.com/education/2022/11/20181328/Emu.jpg",
+            "name" => "Emu",
+            "price" => "200.00",
+            "button" => [
+                "title" => "BUY NOW",
+                "link" => "#",
+            ],
+        ],
+        [
+            "photo" =>
+            "https://cdn.firstcry.com/education/2022/11/20180706/Crows.jpg",
+            "name" => "Crow",
+            "price" => "50.00",
+            "button" => [
+                "title" => "BUY NOW",
+                "link" => "#",
+            ],
+        ],
+        [
+            "photo" =>
+            "https://cdn.firstcry.com/education/2022/11/20180808/Doves.jpg",
+            "name" => "Dove",
+            "price" => "70.00",
+            "button" => [
+                "title" => "BUY NOW",
+                "link" => "#",
+            ],
+        ],
+        [
+            "photo" =>
+            "https://cdn.firstcry.com/education/2022/11/20180808/Doves.jpg",
+            "name" => "Duck",
+            "price" => "120.00",
+            "button" => [
+                "title" => "BUY NOW",
+                "link" => "#",
+            ],
+        ],
+       
+
     ],
 ];
